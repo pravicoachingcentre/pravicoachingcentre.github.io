72ea3d9355e2a358fe83d491793060bc942763c4
pravicoachingcentre <pravicoaching@gmail.com>
2023-07-22 00:01:12 +0530

    go to top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -157,3 +157,18 @@ for (i = 0; i < coll.length; i++) {
 //     }
 //   });
 // }
+
+var btn = $("#button");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
